209151ebc8cffc60bdff4cdc95d9e23577f6c2a6
Murray J Peterson <69125366+photon-flip@users.noreply.github.com>
2024-01-20 12:25:20 +1300

    Update HtmlAccordionModule.php
    Add vanilla template view

--- HtmlAccordionModule.php
+++ HtmlAccordionModule.php
@@ -201,8 +201,9 @@ class HtmlAccordionModule extends HtmlBlockModule implements ModuleCustomInterfa
         $languages      = explode(',', $this->getBlockSetting($block_id, 'languages'));
 
         $templates = [
-            $html                                                       => I18N::translate('Choose Template from the Dropdown'),            
-            view('html/accordion')                     => I18N::translate('Accordion Sections')            
+            $html                                                       => I18N::translate('Choose a Template from the Dropdown'),            
+            view('html/accordion')                     => I18N::translate('Accordion Sections'),
+            view('html/accordion-vanilla')             => I18N::translate('Accordion Vanilla')
         ];
 
         return view('html/config', [
