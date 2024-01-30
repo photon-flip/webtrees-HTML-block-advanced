3023913f3dce6b5705eb5fc2120481864be34584
Murray J Peterson <69125366+photon-flip@users.noreply.github.com>
2024-01-30 16:11:11 +1300

    set up for route to tree url for view accordion.

--- HtmlAccordionModule.php
+++ HtmlAccordionModule.php
@@ -204,8 +204,8 @@ class HtmlAccordionModule extends HtmlBlockModule implements ModuleCustomInterfa
 
         $templates = [
             $html                                                       => I18N::translate('Choose a Template from the Dropdown'),            
-            view('html/accordion')                     => I18N::translate('Accordion Starter'),
-            view('html/accordion-vanilla')             => I18N::translate('Accordion Vanilla')
+            view('html/accordion', ['tree' => $tree])          => I18N::translate('Accordion Starter'),
+            view('html/accordion-vanilla')                     => I18N::translate('Accordion Vanilla')
         ];
 
         return view('html/config', [
