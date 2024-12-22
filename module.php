c56477badc2a23b7d80b2ba5756de5e6a97d17f4
Murray J Peterson <69125366+photon-flip@users.noreply.github.com>
2024-12-23 12:59:21 +1300

    Update module.php
    Make webtrees v2.2 compatible

--- module.php
+++ module.php
@@ -8,6 +8,8 @@ declare(strict_types=1);
 
 namespace HtmlBlockAdvanced;
 
+use Fisharebest\Webtrees\Registry;
+
 require __DIR__ . '/HtmlAdvancedModule.php';
 
-return app(HtmlAdvancedModule::class);
+return Registry::container()->get(HtmlAdvancedModule::class);
